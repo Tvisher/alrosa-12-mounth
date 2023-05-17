ac5d2b976f03dd6ee8af92d9120b7e3cbbc2ae51
Tvisher <tvisher@mail.ru>
2023-05-17 15:33:57 +0600

    add secon screen

--- src/js/app.js
+++ src/js/app.js
@@ -11,7 +11,7 @@ window.addEventListener('load', (e) => {
         throttleDelay: 20, // the delay on throttle used while scrolling the page (advanced)
 
         // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-        offset: 100, // offset (in px) from the original trigger point
+        offset: 30, // offset (in px) from the original trigger point
         delay: 0, // values from 0 to 3000, with step 50ms
         duration: 1200, // values from 0 to 3000, with step 50ms
         easing: 'ease', // default easing for AOS animations

440ee0f86e5ad0db053f9f87013df7808c6ab870
Tvisher <tvisher@mail.ru>
2023-05-19 14:54:52 +0600

    add some fix

--- src/js/app.js
+++ src/js/app.js
@@ -9,14 +9,13 @@ window.addEventListener('load', (e) => {
         disableMutationObserver: false, // disables automatic mutations' detections (advanced)
         debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
         throttleDelay: 20, // the delay on throttle used while scrolling the page (advanced)
-
         // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
         offset: 30, // offset (in px) from the original trigger point
         delay: 0, // values from 0 to 3000, with step 50ms
         duration: 1200, // values from 0 to 3000, with step 50ms
         easing: 'ease', // default easing for AOS animations
-        once: true, // whether animation should happen only once - while scrolling down
-        mirror: false, // whether elements should animate out while scrolling past them
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: true, // whether elements should animate out while scrolling past them
         anchorPlacement: 'top-center', // defines which position of the element regarding to window should trigger the animation
     });
 });
